028c52ad96eb09cd20a618d8e278742e75488050
sooJack <jackpowerryunizin@gmail.com>
2026-05-21 11:04:59 -0300

    atualizações para o github

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import "./styles/global.css";
 import App from "./App.jsx";
@@ -15,9 +15,9 @@ createRoot(document.getElementById("root")).render(
       <LanguageProvider>
         <AuthProvider>
           <ToastProvider>
-            <BrowserRouter>
+            <HashRouter>
               <App />
-            </BrowserRouter>
+            </HashRouter>
           </ToastProvider>
         </AuthProvider>
       </LanguageProvider>
