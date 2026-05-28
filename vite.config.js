c2713779db1e229211de27bafdd31f42643ee2e2
sooJack <jackpowerryunizin@gmail.com>
2026-05-28 09:03:25 -0300

    commit correções de problemas no Github

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === 'production' ? '/seguranca-privada-rm-rh/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
